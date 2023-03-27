5725232390d20e26a748e518e018728793fffccd
Shimul Das <shimul.191002110@gmail.com>
2023-03-27 15:12:35 +0600

    data fecth in react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-<Counter></Counter>
+{/* <Counter></Counter> */}
+<ExternalUser></ExternalUser>
    
     </div>
   );
 }
 
+function ExternalUser(){
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+  },[])
+  return(
+    <div>
+      <h2>External User</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user=><User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props){
+return(
+  <div style={{border:"1px solid red", margin:'50px'}}>
+    <h3>Name:{props.name}</h3>
+    <h4>Email:{props.email}</h4>
+  </div>
+)
+}
 function Counter(){
   const [count, setCount]=useState(0)
   // const increaseCount=()=>{
